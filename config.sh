5cb76dd307a44ff9cc532ae0c42b6e0c2bf138d5
Shwetank Rai <60915576+ishugrapher@users.noreply.github.com>
2023-04-13 08:34:45 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@ cd riceOSS
 git config --global user.email "shwetankrai12@gmail.com" && git config --global user.name "Shwetank Rai"
 repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --force-remove-dirty --optimized-fetch --prune --force-sync -j8
-git clone https://github.com/realme-mt6893-dev/android_device_realme_RMX3031 -b thirteen device/realme/RMX3031
+git clone https://github.com/ishugrapher/android_device_realme_RMX3031 -b thirteen device/realme/RMX3031
 git clone https://github.com/realme-mt6893-dev/android_kernel_realme_mt6893 -b thirteen kernel/realme/mt6893
 git clone https://github.com/realme-mt6893-dev/proprietary_vendor_realme_RMX3031 -b thirteen vendor/realme/RMX3031
 git clone https://github.com/ManshuTyagi/vendor_realme_RMX3031-firmware vendor/realme/RMX3031-firmware 
